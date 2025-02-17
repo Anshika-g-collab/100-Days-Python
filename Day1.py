a3e2fe7d752453b4180ac016641c21d34c9f5292
Anshika-g-collab <anshikapunase@gmail.com>
2025-02-17 12:37:25 +0530

    printing and variables

--- Day1.py
+++ Day1.py
@@ -1,1 +1,8 @@
 print("Hello world!")
+print("Hello world!\nHello world!")
+#string concatenation!
+print("Hello!" + input("What is your name?") )
+name = input("Enter your name..")
+print(name)
+length = len(name)
+print(length)
