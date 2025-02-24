9eb6211139d929fce4eaf907d4403a7d6ae62b39
Anshika-g-collab <anshikapunase@gmail.com>
2025-02-24 15:11:24 +0530

    BMI

--- Day3/BMI.py
+++ Day3/BMI.py
@@ -2,12 +2,12 @@ height = float(input("Enter your height in m: "))
 weight = float(input("Enter your weight in kg: "))
 BMI = weight/(height**2)
 if BMI < 18.5:
-    print("You are 'underweight'!")
+    print(f"Your BMI is {BMI} , You are 'underweight'!")
 elif BMI > 18.5 and BMI<25:
-    print("You have 'normal weight'!")
+    print(f"Your BMI is {BMI} , You have 'normal weight'!")
 elif BMI >25 and BMI<30:
-    print("You are 'overweight'!")
+    print(f"Your BMI is {BMI} , You are 'overweight'!")
 elif BMI > 30 and BMI <35:
-    print("You are 'obese'!")
+    print(f"Your BMI is {BMI} , You are 'obese'!")
 else:
-    print("You are 'clinically obese'!")
+    print(f"Your BMI is {BMI} , You are 'clinically obese'!")
