88805413e90c9c4890d125658a2541f80a2cd8d6
Anshika-g-collab <anshikapunase@gmail.com>
2025-02-26 22:45:35 +0530

    Rock Paper Scissors

--- Project4.py
+++ Project4.py
@@ -31,34 +31,37 @@ Scissors = ("""
 """)
 game_img = [Rock,Paper,Scissors]
 user_choice = int(input(" Choose '0' for rock , '1' for paper and '2' for Scissors "))
-print("You chose: ")
-print(game_img[user_choice])
 
-computer_choice = random.randint(0,2)
-print("Computer chose: ")
-print(game_img[computer_choice])
+if user_choice>0 and user_choice<3:
+    print("You chose: ")
+    print(game_img[user_choice])
+    computer_choice = random.randint(0,2)
+    print("Computer chose: ")
+    print(game_img[computer_choice])
 
-if user_choice == 0:
-    if computer_choice ==0:
-        print("Game Draw! 😏")
-    elif computer_choice == 1:
-        print("You Lose! 🤐")
-    else:
-        print("You Win! 😎")
+    if user_choice == 0:
+        if computer_choice ==0:
+            print("Game Draw! 😏")
+        elif computer_choice == 1:
+            print("You Lose! 🤐")
+        else:
+            print("You Win! 😎")
 
-if user_choice == 1:
-    if computer_choice ==0:
-        print("You Win! 😎")
-    elif computer_choice == 1:
-        print("Game Draw! 😏")
-    else:
-        print("You Lose! 🤐")
+    if user_choice == 1:
+        if computer_choice ==0:
+            print("You Win! 😎")
+        elif computer_choice == 1:
+            print("Game Draw! 😏")
+        else:
+            print("You Lose! 🤐")
 
-if user_choice == 2:
-    if computer_choice ==0:
-        print("You Lose! 🤐")
-    elif computer_choice == 1:
-        print("You Win! 😎")
-    else:
-        print("Game Draw! 😏")
+    if user_choice == 2:
+        if computer_choice ==0:
+            print("You Lose! 🤐")
+        elif computer_choice == 1:
+            print("You Win! 😎")
+        else:
+            print("Game Draw! 😏")
 
+else:
+    print("Invalid Choice!")
