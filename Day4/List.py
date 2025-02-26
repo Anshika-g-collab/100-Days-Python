d3c19ebc88899f29e38b224055f79f60e4aaed1b
Anshika-g-collab <anshikapunase@gmail.com>
2025-02-26 20:59:21 +0530

    Nested List

--- Day4/List.py
+++ Day4/List.py
@@ -11,3 +11,14 @@ print(states_of_India)
 # .extend(iterable) = a[len(a):] = iterable
 states_of_India.extend(['Kerela',"Manipur","Telengana"])
 print(states_of_India)
+
+#IndexError - print(state_of_India[12])
+
+#Nested list
+# dirty_dozens = ['Strawberries','Nectarines','Apples',"Grapes","Peaches","Cherries","Pears","Spinach","Tomatoes","Celery","Potatoes","kale"]
+
+fruits = ['Strawberries','Nectarines','Apples',"Grapes","Peaches","Cherries","Pears"]
+vegetables = ["Spinach","Tomatoes","Celery","Potatoes","kale"]
+
+dirty_dozens = [fruits,vegetables]
+print(dirty_dozens)
