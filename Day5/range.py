cd98d94d3207f854978e6e18f384cf9677240390
Anshika-g-collab <anshikapunase@gmail.com>
2025-03-01 20:38:17 +0530

    Range

---
+++ Day5/range.py
@@ -0,0 +1,8 @@
+# printing odd numbers
+for number in range(1,11,2):
+    print(number)
+# adding range of numbers
+total = 0
+for number in range(1,101):
+    total+=number
+print(f"total is : {total}" )
