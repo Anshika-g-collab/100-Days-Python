7406fc36fd96260f0d8803c1a7273de717ce636c
Anshika-g-collab <anshikapunase@gmail.com>
2025-03-02 13:52:26 +0530

    Hangman

--- Project6.py
+++ Project6.py
@@ -2,21 +2,29 @@ import random
 
 word_list = ["camel","killer","awkward","galaxy","rhythm",'force',"tug","borrow","antibacterial","cake"]
 
-#TODO_1 - Randomly choose a word from the word_list and assign it to a variable called choosen_word.
+# Randomly choose a word from the word_list and assign it to a variable called choosen_word.
 
 choosen_word = random.choice(word_list)
 
-for letter in choosen_word:
-     print("_",end=" ")
+# Create an empty list called 'Display' .
+# for each letter in the choosen_word , add a "_" to 'display'.
 
-#TODO_2 - ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+display = []
+for letter in choosen_word:
+     display.append("_")
+print(display)
+
+# ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 guess = input("\nGuess a letter: ").lower()
 
-#TODO_3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# print(choosen_word).
 
-for letter in choosen_word:
-    if letter == guess:
-         print("Right")
-    else:
-         print("Wrong")
+for n in range(0,len(choosen_word)):
+    if choosen_word[n] == guess:
+         display[n] = guess
+
+print(display)
+         
+    
