b9f2e72ad7f55634d2baddc22671c5f0b3d089c1
Anshika-g-collab <anshikapunase@gmail.com>
2025-03-02 11:20:38 +0530

    Add all evens

---
+++ Day5/AddEvens.py
@@ -0,0 +1,5 @@
+sum_even_numbers = 0
+for even_numbers in range(2,101,2):
+    # print(even_numbers,end=" ")
+    sum_even_numbers +=even_numbers
+print(f"Sum of all even numbers from 1 to 100 is : {sum_even_numbers}")
