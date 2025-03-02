cd7c422ee8e1ac0df82eadcfaee2b751b1bcb99a
Anshika-g-collab <anshikapunase@gmail.com>
2025-03-02 18:13:54 +0530

    Positional Vs Keyword args

--- Day7/functionParameters.py
+++ Day7/functionParameters.py
@@ -3,3 +3,12 @@ def greet(name):
     print(f"Good morning! {name}.")
 
 greet("Anshika")
+
+# function with more than 1 input
+def greet_with(name,location):
+    print(f'Hello {name}!')
+    print(f'What is it like in {location}?')
+#positional arguments
+greet_with("Anshika","Nowhere")
+#Keyword arguments
+greet_with(location = "Greece" , name = "Anshika")
