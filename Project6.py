73565bec9e131005e5a04302fe4545afe281925e
Anshika-g-collab <anshikapunase@gmail.com>
2025-03-02 15:28:07 +0530

    Hangman

--- Project6.py
+++ Project6.py
@@ -1,30 +1,102 @@
 import random 
-
+# Raw strings (r"") tell Python to ignore escape sequences.
+stages = [r'''
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+===========
+''',
+r'''
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+===========
+''',
+r'''
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+===========
+''',
+r'''
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+===========
+''',
+r'''
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+===========
+''',
+r'''
+    +---+
+    |   |
+    O   |
+   /|\  |
+   /    |
+        |
+===========
+''',
+r'''
+    +---+
+    |   |
+    O   |
+   /|\  |
+   / \  |
+        |
+===========
+''']
 word_list = ["camel","killer","awkward","galaxy","rhythm",'force',"tug","borrow","antibacterial","cake"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+end_of_game = False
 
-# Randomly choose a word from the word_list and assign it to a variable called choosen_word.
+lives = 0
 
-choosen_word = random.choice(word_list)
-
-# Create an empty list called 'Display' .
-# for each letter in the choosen_word , add a "_" to 'display'.
-
+print("🎮 Welcome to Hangman! 🎮")
 display = []
-for letter in choosen_word:
-     display.append("_")
-print(display)
+for letter in chosen_word:
+     display+="_"
+print(f"{' '.join(display)}")
+print(stages[lives])
+while not end_of_game :
+    
+    guess = input("\nGuess a letter: ").lower()
 
-# ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+    for n in range(word_length):
+        if chosen_word[n] == guess:
+            display[n] = guess
+            print(f" You guessed it right 👏")
+    if guess not in chosen_word:
+        lives+=1
+        print(f"❌ Incorrect guess! You have {6-lives} lives left.")
+        if lives == 6:
+            end_of_game = True
+            print(f"💀 Game Over! The word was: {chosen_word}")
+    print(f"{' '.join(display)}")
+    
+    if '_' not in display:
+        end_of_game = True
+        print("🎉 Congratulations! You guessed the word! 🎉")
 
-guess = input("\nGuess a letter: ").lower()
+    print(stages[lives])
 
-# Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-# print(choosen_word).
-
-for n in range(0,len(choosen_word)):
-    if choosen_word[n] == guess:
-         display[n] = guess
-
-print(display)
          
     
