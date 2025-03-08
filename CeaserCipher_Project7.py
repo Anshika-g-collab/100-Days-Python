4fbb1eb8496eb3674282cc095798fcb4ab10f31f
Anshika-g-collab <anshikapunase@gmail.com>
2025-03-08 23:33:53 +0530

    Increase efficiency

--- CeaserCipher_Project7.py
+++ CeaserCipher_Project7.py
@@ -1,34 +1,20 @@
 alphabet = ['a' , 'b' ,'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-direction = input("Type 'encode' to encrypt ,\nType 'decode' tp decrypt: \n").lower()
+direction = input("Type 'encode' to encrypt ,\nType 'decode' to decrypt: \n").lower()
 
 text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(plain_text,shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position+shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text+=new_letter
-    print(f"The encoded text is {cipher_text}")
+def ceaser(start_text,shift_amount,cipher_direction):
+    if cipher_direction == 'decode':
+        shift_amount*=-1
+    end_text=""
+    for letter in start_text:
+        position=alphabet.index(letter)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction}d text is {end_text}")
 
-
-def decrypt(cipher_text,shift_amount):
-    plain_text =""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position-shift_amount
-        new_letter = alphabet[new_position]
-        plain_text+=new_letter
-    print(f"The decoded text is {plain_text}")
-
-if direction=='encode':
-    encrypt(plain_text=text,shift_amount=shift)
-elif direction == 'decode':
-    decrypt(cipher_text=text,shift_amount=shift)
-else:
-    print("No such function available , try again!")
+ceaser(start_text=text,shift_amount=shift,cipher_direction=direction)
 
